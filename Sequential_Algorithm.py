c2f82808dadefe2ec79ff6ae36be242fda42655c
AjayPal2008 <128708345+AjayPal2008@users.noreply.github.com>
2026-08-31 13:47:09 -0500

    Add files via upload

---
+++ Sequential_Algorithm.py
@@ -0,0 +1,28 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: Ajay P
+# Section: ENGR 102 508
+# Assignment: LAB 0
+# Date: 08/31/2026
+# E.King Gill statue from Zach 240
+
+print("Leave Zach 240")
+print("Take a left and walk towards starbucks")
+print("After crossing starbucks, go down stairs")
+print("Turn Left to the main exit and exit on Spence Street")
+print("walk straight on Spence st. till you reach  Liberal Arts and Humanities Building")
+print("turn right onto ThrockMorton Stree")
+print("walk until you reach Sterling C. Evans Library")
+print("turn left and walk  on the sidewalk till you reach Lamar st.")
+print("turn right onto Lamar Street")
+print("walk until you reach the Department of Biology")
+print("turn Left onto Nagle Street")
+print("Walk until you reach an intersection")
+print("Turn Right onto Lubbock st.")
+print("Walk straight till you reach The Rudder Theatre Complex")
+print("Turn Left onto ThrockMorton Street")
+print("Walk straight till you reach the intersection")
+print("Turn onto Joe Routt Blvd.")
+print("You will find E.King Gill statue  on the Left upon crossing Texas A and M career center")
