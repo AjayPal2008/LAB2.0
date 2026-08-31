4e4885fd439eebb7a1103224e146afe41783cba0
hudsonrdobbs <hudsonrdobbs@tamu.edu>
2026-08-31 14:26:23 -0500

    Update Sequential_Algorithm.py

--- Sequential_Algorithm.py
+++ Sequential_Algorithm.py
@@ -12,22 +12,22 @@
 # Date:         31 August 2026
 
 # (Ajay) E.King Gill statue from Zach 240
-print("Leave Zach 240") # gga
+print("Leave Zach 240") 
 print("Take a left and walk towards starbucks")
-print("After crossing starbucks, go down stairs")
+print("After crossing starbucks, go down stairs") # passing can be used instead of crossing
 print("Turn Left to the main exit and exit on Spence Street")
-print("walk straight on Spence st. till you reach  Liberal Arts and Humanities Building")
+print("walk straight on Spence st. till you reach Liberal Arts and Humanities Building") # straight which direction
 print("turn right onto ThrockMorton Stree")
 print("walk until you reach Sterling C. Evans Library")
 print("turn left and walk  on the sidewalk till you reach Lamar st.")
 print("turn right onto Lamar Street")
-print("walk until you reach the Department of Biology")
+print("walk until you reach the Department of Biology") # department of biology building?
 print("turn Left onto Nagle Street")
-print("Walk until you reach an intersection")
+print("Walk until you reach an intersection") # which intersection?
 print("Turn Right onto Lubbock st.")
 print("Walk straight till you reach The Rudder Theatre Complex")
 print("Turn Left onto ThrockMorton Street")
-print("Walk straight till you reach the intersection")
+print("Walk straight till you reach the intersection")  # which intersection?
 print("Turn onto Joe Routt Blvd.")
 print("You will find E.King Gill statue  on the Left upon crossing Texas A and M career center")
 
@@ -50,24 +50,24 @@ print("Instantly take a left and walk towards the Starbucks.")
 print("Once you reach the Starbucks, take the half flight of stars down to the first floor where you will find the atrium.")
 print("Once at the atrium, take a left to the main exit and onto Spence Street.")
 print("Take a right onto Ross Street.")
-print("Continue on Ross Street, and you will see that it will slowly take a right ahead and it will intersect with Houston Street on the left. Take the pathway on the left right before Houston Street.")
+print("Continue on Ross Street, and you will see that it will slowly take a right ahead and it will intersect with Houston Street on the left. Take the pathway on the left right before Houston Street.") # very long instructions
 print("Continue down the pathway until you meet the James Earl Rudder statue. From there you will turn right.")
 print("Continue until you reach Throckmorton Street, and then take a right.")
 print("Stay on Throckmorton Street until you reach Joe Routt Boulevard, and take a right onto it.")
 print("Continue on Joe Routt Boulevard, pass Houston Street on the left, then turn a left right after you pass Houston Street.")
-print("You have arrived at the E. King Hill statue.")
+print("You have arrived at the E. King Hill statue.") # good directions overall
 
-#Steps to get to the giant Aggie ring statue in Haynes Ring Plaza from ZACH-240 - Micah
+# (Micah) Steps to get to the giant Aggie ring statue in Haynes Ring Plaza from ZACH-240
 print ("Step out of room ZACH-240")
 print ("Immediately take a left and walk towards where the starbucks")
 print ("Once you reach the starbucks take the half flight of stairs down to the main floor whee you will reach the atrium")
 print ("Once at the atrium take a left to the main exit and exit on spence street")
 print ("When you are on spence street take a left and begin walking down the street")
-print ("To know if you are on the right path you should pass building such as the building of Mechanical Engineering and Harey R. Bright Building")
-print ("Continue walking straight until you see the Anthropology Building where you will go a bit past it and take a right, down the path at the back")
+print ("To know if you are on the right path you should pass building such as the building of Mechanical Engineering and Harey R. Bright Building") # good specific directions
+print ("Continue walking straight until you see the Anthropology Building where you will go a bit past it and take a right, down the path at the back") # how far is a bit?
 print ("You will then take continue walking straight till you come across a path to your left between Glasscock Building and Sterling C.Evans Library")
 print ("Continue walking till you reach the end of the first section of the library and take a right and walk straight down to the Biological Sciences Building Bldg East")
 print ("When you reach the building take a left and walk straight down till you reach the general road again, then walk northeast down to Coke Street")
-print ("Walk straight down Coke Street past building such as the Sanders Corps of Cadets Centre, the Memorial bench.")
-print ("Continue Walking till you reach the intersections and make your way to George Bush Dr")
-print ("Walk down till you see the Association of Former students building and there you would should the Giant Aggie Ring")
+print ("Walk straight down Coke Street past buildings such as the Sanders Corps of Cadets Centre, and the Memorial bench.")
+print ("Continue Walking till you reach the intersections and make your way to George Bush Dr") # which intersection?
+print ("Walk down till you see the Association of Former students building and there you will see the Giant Aggie Ring") 
