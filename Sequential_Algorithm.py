2777c11c13489d8fc0ab6bd2fc1603cc1c3bb9fa
benjaminhatch <benjaminhatch@tamu.edu>
2026-08-31 13:56:35 -0500

    Remove duplicate comments and print statements

--- Sequential_Algorithm.py
+++ Sequential_Algorithm.py
@@ -43,3 +43,16 @@ print("Once at the Fish pond, look 90 degrees to the left and cross Ross Street 
 print("Once on the sidewalk along Houston Street, walk towards the MSC for nearly a 1/4 mile")
 print("After passing the Student Services Building take a left")
 print("Walk 20ft along this path and placed just to the right of the sidewalk is the Matthew Gaines Statue")
+
+# (Benjamin) Instructions to go from ZACH240 to E. King Hill statue.
+print("Leave ZACH240.")
+print("Instantly take a left and walk towards the Starbucks.")
+print("Once you reach the Starbucks, take the half flight of stars down to the first floor where you will find the atrium.")
+print("Once at the atrium, take a left to the main exit and onto Spence Street.")
+print("Take a right onto Ross Street.")
+print("Continue on Ross Street, and you will see that it will slowly take a right ahead and it will intersect with Houston Street on the left. Take the pathway on the left right before Houston Street.")
+print("Continue down the pathway until you meet the James Earl Rudder statue. From there you will turn right.")
+print("Continue until you reach Throckmorton Street, and then take a right.")
+print("Stay on Throckmorton Street until you reach Joe Routt Boulevard, and take a right onto it.")
+print("Continue on Joe Routt Boulevard, pass Houston Street on the left, then turn a left right after you pass Houston Street.")
+print("You have arrived at the E. King Hill statue.")
