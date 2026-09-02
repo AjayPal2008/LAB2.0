53ce6b4261707cbd50f153a17ce901e8f2f83a29
AjayPal2008 <128708345+AjayPal2008@users.noreply.github.com>
2026-09-01 22:04:17 -0500

    Update Sequential_Algorithm.py

--- Sequential_Algorithm.py
+++ Sequential_Algorithm.py
@@ -32,26 +32,26 @@ print("You will find E.King Gill statue  on the Left upon crossing Texas A and M
 
 # (Hudson) Instructions to go from ZACH Exit on Spence Street to Matthew Gaines Statue
 print("Leave ZACH 240")
-print("Immediately take a left and walk towards where the starbucks")
+print("Immediately take a left and walk towards where the starbucks")# actually you need to walk past starbucks
 print("Once you reach the Starbucks take the half flight of stairs down to the main floor where you will reach the atrium")
 print("Once at the atrium take a left to the main exit and exit on Spence street")
 print("Walk from the Exit of ZACH 240 on Spence street to the sidewalk along Spence Street heading towards Ross Street")
 print("Once at the Intersection of Spence Street and Ross Street, take a right on Ross Street")
-print("Follow the sidewalk along Ross Street towards Houston Street")
+print("Follow the sidewalk along Ross Street towards Houston Street")# how will the users know which direction is houston street?
 print("Once at the Fish pond, look 90 degrees to the left and cross Ross Street to be on Houston Street")
 print("Once on the sidewalk along Houston Street, walk towards the MSC for nearly a 1/4 mile") #how will the walker automatically know which direction the MSC is in?
-print("After passing the Student Services Building, take a left")
+print("After passing the Student Services Building, take a left")# how does the building look?
 print("Walk 20ft along this path and placed just to the right of the sidewalk is the Matthew Gaines Statue") #other than the note above, clear and concise directions.
 
 # (Benjamin) Instructions to go from ZACH240 to E. King Hill statue.
 print("Leave ZACH240.")
 print("Instantly take a left and walk towards the Starbucks.")
-print("Once you reach the Starbucks, take the half flight of stars down to the first floor where you will find the atrium.")
+print("Once you reach the Starbucks, take the half flight of stars down to the first floor where you will find the atrium.")#I think it is full stairs
 print("Once at the atrium, take a left to the main exit and onto Spence Street.")
-print("Take a right onto Ross Street.")
+print("Take a right onto Ross Street.")# need a marker for them to know when to take right exactly
 print("Continue on Ross Street, and you will see that it will slowly take a right ahead and it will intersect with Houston Street on the left. Take the pathway on the left right before Houston Street.") # very long instructions
 print("Continue down the pathway until you meet the James Earl Rudder statue. From there you will turn right.")
-print("Continue until you reach Throckmorton Street, and then take a right.")
+print("Continue until you reach Throckmorton Street, and then take a right.")# how will they know the street is thorckmorton street?
 print("Stay on Throckmorton Street until you reach Joe Routt Boulevard, and take a right onto it.")
 print("Continue on Joe Routt Boulevard, pass Houston Street on the left, then turn a left right after you pass Houston Street.")
 print("You have arrived at the E. King Hill statue.") # good directions overall
@@ -64,9 +64,9 @@ print ("Once at the atrium take a left to the main exit and exit on spence stree
 print ("When you are on spence street take a left and begin walking down the street")
 print ("To know if you are on the right path you should pass building such as the building of Mechanical Engineering and Harey R. Bright Building") # good specific directions
 print ("Continue walking straight until you see the Anthropology Building where you will go a bit past it and take a right, down the path at the back") # how far is a bit?
-print ("You will then take continue walking straight till you come across a path to your left between Glasscock Building and Sterling C.Evans Library")
+print ("You will then take continue walking straight till you come across a path to your left between Glasscock Building and Sterling C.Evans Library")# more specific the area has many crisscrossing roads
 print ("Continue walking till you reach the end of the first section of the library and take a right and walk straight down to the Biological Sciences Building Bldg East")
 print ("When you reach the building take a left and walk straight down till you reach the general road again, then walk northeast down to Coke Street")
 print ("Walk straight down Coke Street past buildings such as the Sanders Corps of Cadets Centre, and the Memorial bench.")
 print ("Continue Walking till you reach the intersections and make your way to George Bush Dr") # which intersection?
-print ("Walk down till you see the Association of Former students building and there you will see the Giant Aggie Ring") 
+print ("Walk down till you see the Association of Former students building and there you will see the Giant Aggie Ring")# Describe the ring so it can be identified easier
