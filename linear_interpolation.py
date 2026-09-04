0936c300ce803ceb72a5ada851ac2b7876641522
AjayPal2008 <128708345+AjayPal2008@users.noreply.github.com>
2026-09-04 13:32:26 -0500

    Update linear_interpolation.py

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -4,6 +4,6 @@ final_distance = 23030
 final_time = 55
 slope = (final_distance - initial_distance) / (final_time - initial_time)
 y_intercept = initial_distance - slope * initial_time
-time = float(input("Enter a time in minutes to calculate the distance traveled: "))
-print("The distance traveled is", (slope * time + y_intercept)%42376.6, "kms")
+time = 25 #float(input("Enter a time in minutes to calculate the distance traveled: "))
+print("The distance traveled is", (slope * time + y_intercept)%42376.6, "kms")# for part one put in 25 mins as Input remove input function.
 
