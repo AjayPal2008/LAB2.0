6fb30b49f2ecaff44cdb14648c600a42c81dbad9
AjayPal2008 <128708345+AjayPal2008@users.noreply.github.com>
2026-09-04 14:16:41 -0500

    Refactor comments and update date in linear_interpolation.py
    Updated comments and formatting for clarity.

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -2,28 +2,23 @@
 #   "Aggies do not lie, cheat, or steal, or tolerate those who do."
 #   "I have not given or received any unauthorized aid on this assignment."
 #
-# Names:        Micah Kadiri
-#               Benjamin Hatch
-#               Ajay Palanisamy
-#               Hudson Dobbs
-# Section:      508
-# Assignment:   Lab Topic 2 (Team: Part 1)
-# Date:         31 August 2026
+# Names:                    Micah Kadiri
+#                           Benjamin Hatch
+#                           Ajay Palanisamy
+#                           Hudson Dobbs
+# Section:            508
+# Assignment:    Lab Topic 2 (Team: Part 1)
+# Date:                4 september 2026
+
 
 initial_distance = 2030 
 initial_time = 10
 final_distance = 23030
 final_time = 55
-slope = (final_distance - initial_distance) / (final_time - initial_time)
+slope = (final_distance - initial_distance) / (final_time - initial_time)#calculates the slope of the line
 y_intercept = initial_distance - slope * initial_time
-time = 25 #float(input("Enter a time in minutes to calculate the distance traveled: "))
-print("The distance traveled is", (slope * time + y_intercept)%42376.6, "kms")# for part one put in 25 mins as Input remove input function.
 
-#Part 1 
-i_d = 2030 #kilometres
-i_t = 10
-f_d = 23030 #kilometres
-f_t = 55
-distance = float(input("Input a number between 10 and 55 -"))
-x_2 = 466.6676*distance - 2636.67
-print("For t=" ,distance, "minutes, the position p =" ,x_2, "kilometres")
+
+
+print("Part 1:\nFor t = 25 minutes, the position p =", (slope * 25 + y_intercept), "kilometers")
+print("Part 2:\nFor t = 300 minutes, the position p =", (slope * 300 + y_intercept)%42380.19, "kilometers")
